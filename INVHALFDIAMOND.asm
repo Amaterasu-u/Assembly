2ea28aa4c119632e6e8f9a68c8019d3bd6cc12f2
Yeaser Ahmad <yeaser.007ahmad@gmail.com>
2025-08-25 23:15:14 +0600

    Add files via upload

--- INVHALFDIAMOND.asm
+++ INVHALFDIAMOND.asm
@@ -12,7 +12,9 @@
  HOLDN DB 0  
  LEVEL DB 0 
  ROWS DB 0
- 
+  SPACES DB 0
+  
+  
 .CODE
 
 MAIN PROC
@@ -24,37 +26,97 @@ MOV N1,AL
 MOV N2,AL
 MOV N3,AL
 
+MOV I,2
+MUL I
+SUB AL,1
+MOV ROWS,AL
+
+MOV AH,02H
+MOV DL,0DH
+INT 21H
+MOV DL,0AH
+INT 21H
+
+
+
+
 
 
 MOV CL,1
 
 LUPI:
 
-CMP CL,N
+CMP CL,ROWS 
+JG TERMINATE
+  
+  
+  
+ MOV LEVEL,0 
+ CMP CL,N1
+ JG P1
+ MOV LEVEL,CL
+ JMP PROCEED
+ 
+ P1:
+ 
+ MOV CH,ROWS
+ SUB CH,CL
+ ADD CH,1
+ MOV LEVEL,CH
+   
+ PROCEED:
+ 
+ MOV CH,N1
+ SUB CH,LEVEL
+ MOV SPACES,CH
+ 
+ 
+ MOV BL,0
+ LUPJ:
+ 
+ CMP BL,SPACES
+ JGE NEXTLOOP   
+    
+ MOV AH,02H
+ MOV DL,' '
+ INT 21H
+ 
+ INC BL
+ JMP LUPJ   
+    
+   
+NEXTLOOP:    
+    
+
+
+    MOV BH,0
+    MOV DL,'1'       
+    
+
+LUPK:
+    CMP BH,LEVEL
+    JGE OUTER
+    MOV AH,02H
+    INT 21H         
+    XOR DL, 1  
+
+
+    INC BH
+    JMP LUPK
+
+ 
+ OUTER:
+   
+ MOV AH,02H
+MOV DL,0DH
+INT 21H
+MOV DL,0AH
+INT 21H   
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+           INC CL
+           JMP LUPI
+ TERMINATE:   
     
     
     MAIN ENDP
